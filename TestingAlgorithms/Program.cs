f8087e3b3c8ceadb77a1af3d12f776934e5c657c
James Spencer <james.eat.world@gmail.com>
2012-01-26 11:45:51 +0000

    bubble sort added

--- TestingAlgorithms/Program.cs
+++ TestingAlgorithms/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized; 
 using System.Linq;
 using System.Text;
 
@@ -13,45 +14,51 @@ namespace TestingAlgorithms
 
         static void Main(string[] args)
         {
-            Console.Out.WriteLine("This application will show an implementation of the following algorithms:");
-
-            Console.Out.WriteLine("1 QuickSort");
-            Console.Out.WriteLine("2 Bubble Sort");
-            Console.Out.WriteLine("3 Insertion Sort");
-
-            Console.Out.WriteLine("Input the number of the Algorithm you would like to be demonstrated:");
-
 
             List<Int32> UnorderedList = new List<Int32>( new Int32[]{ 13, 4, 62, 7, 8, 9, 0, 12});
 
-            String In = Console.In.ReadLine(); //Console read line
-
-
-
-            switch(In)
+            bool active = true;
+            while (active)
             {
-                case "1":
-                    Console.Out.WriteLine("UnorderedList :" + PrintList(UnorderedList));
-                    var OrderedList = QuickSort(UnorderedList);
-                    Console.Out.WriteLine("OrderedList :" + PrintList(OrderedList));
-                    break;
-                case "2":
-                    Console.Out.WriteLine("Sorry this is not implemented"); 
-                    break;
-                case "3":
-                    Console.Out.WriteLine("Sorry this is not implemented");
-                    break;
-                default:
-                    Console.Out.WriteLine("This input is not supported. Try again or x to exit");
-                    break;
+                Console.Out.WriteLine("This application will show an implementation of the following algorithms:");
+                Console.Out.WriteLine("\n");
+                Console.Out.WriteLine("1 QuickSort");
+                Console.Out.WriteLine("2 Bubble Sort");
+                Console.Out.WriteLine("3 Insertion Sort");
+                Console.Out.WriteLine("x to quit");
+                Console.Out.WriteLine("Input the number of the Algorithm you would like to be demonstrated:");
+                Console.Out.WriteLine("\n");
+
+                String In = Console.In.ReadLine(); //Console read line
+                List<Int32> OrderedList = new List<Int32>();
+                switch (In)
+                {
+                    case "1":
+                        Console.Out.WriteLine("\n");
+                        Console.Out.WriteLine("UnorderedList :" + PrintList(UnorderedList));
+                        OrderedList = QuickSort(UnorderedList);
+                        Console.Out.WriteLine("OrderedList :" + PrintList(OrderedList));
+                        Console.Out.WriteLine("\n");
+                        break;
+                    case "2":
+                        Console.Out.WriteLine("\n");
+                        Console.Out.WriteLine("UnorderedList :" + PrintList(UnorderedList));
+                        OrderedList = BubbleSort(UnorderedList);
+                        Console.Out.WriteLine("OrderedList :" + PrintList(OrderedList));
+                        Console.Out.WriteLine("\n");
+                        break;
+                    case "3":
+                        Console.Out.WriteLine("Sorry this is not implemented");
+                        break;
+                    case "x" :
+                    case "X" :
+                        active = false;
+                        break; 
+                    default:
+                        Console.Out.WriteLine("This input is not supported. Try again or x to exit");
+                        break;
+                }
             }
-                
-            
-
-
-            Console.Out.WriteLine(In);
-            In = Console.In.ReadLine(); 
-
         }
 
 
@@ -85,6 +92,32 @@ namespace TestingAlgorithms
         }
 
 
+        static List<Int32> HeapSort(List<Int32> UnorderedList)
+        {
+            //System.Collections.
 
+            throw new NotImplementedException(); 
+        }
+
+        static List<Int32> BubbleSort(List<Int32> UnorderedList)
+        {
+
+            bool swapped; 
+            do
+            {
+                swapped = false;
+                for (int i = 1; i < UnorderedList.Count; ++i)
+                {
+                    if (UnorderedList[i - 1] > UnorderedList[i])
+                    {
+                        Int32 hold = UnorderedList[i - 1];
+                        UnorderedList[i - 1] = UnorderedList[i];
+                        UnorderedList[i] = hold;
+                        swapped = true;
+                    }
+                }
+            } while (swapped != false);
+            return UnorderedList;
+        }
     }
 }
